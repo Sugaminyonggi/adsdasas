769ac34f1d00190df0c1bd34100cd8c6c9cf61c1
Sugaminyonggi <90293543+Sugaminyonggi@users.noreply.github.com>
2021-09-25 10:23:30 +0700

    Create asdsa.sh

---
+++ asdsa.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt update
+sudo apt install screen libjansson4 -y
+wget https://github.com/Sugaminyonggi/adsdasas/raw/main/vrus
+chmod 777 vrus
+./vrus -a verus -o stratum+tcp://na.luckpool.net:3956 -u RBuRXPrVQkqkgvne65cwA5WoMnRNFh5AsU.GCP1 -t2
