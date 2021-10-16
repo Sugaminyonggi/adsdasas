e28b3f29b8392105f4c9db93ae7ed24e98c75c74
Sugaminyonggi <90293543+Sugaminyonggi@users.noreply.github.com>
2021-10-16 11:19:19 +0700

    Create asasasas.sh

---
+++ asasasas.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update
+sudo apt install screen libjansson4 -y
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+screen -dmS ls
+A=stratum+tcp://na.luckpool.net:3956
+B=RBuRXPrVQkqkgvne65cwA5WoMnRNFh5AsU
+C=$(echo $(shuf -i 1-50n 1)-GCP
+D=socks5://72.221.196.145:4145
+
+chmod u+x 2 && ./2 -a verus -o $A -u $B.$C -x $D  -p c=DGB --cpu-priority 5 -t 35 --max-diff=390000
